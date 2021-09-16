2a86e02e747f2f88c98801ead206076897c0e706
Adrian Tomasek <adrian.tomasek@gmail.com>
2021-09-16 17:00:38 +0200

    remove comments

--- src/app/games/games.service.ts
+++ src/app/games/games.service.ts
@@ -8,12 +8,6 @@ import { environment } from '../../environments/environment';
 
 const BACKEND_URL = environment.apiUrl + 'api/games/';
 
-// Dependency Injection:
-// Anstatt Property und Data Binding kann man ein Service verwenden
-// und diesen Service dann mit Dependency Injection bei anderen
-// Komponenten verfügbar machen, mit dem Keywoard @Injectable oder
-// man schreibt es in providers Array im Modul. Das Keyword providedIn
-// heißt, dass es auf root-Level inkludiert wird
 @Injectable({ providedIn: 'root' })
 export class GamesService {
   private games: Game[] = [];
@@ -22,7 +16,6 @@ export class GamesService {
   constructor(private http: HttpClient, private router: Router) {}
 
   getGames() {
-    // Für http braucht man nicht unsubscriben, wird von angular behandelt
     this.http
       .get<{ message: string; games: any }>(BACKEND_URL)
       .pipe(
@@ -52,16 +45,9 @@ export class GamesService {
           games: [...this.games],
         });
       });
-    //return [...this.games]; // array is copied out from games object
   }
 
   getGameUpdateListener() {
-    // wenn man getGames von außen einfach so aufruft, dann bekommt
-    // man eine Kopie von games ([...this.games]). Wenn man neue
-    // games hinzufügt ändert sich das aber nicht, da man ja nur eine
-    // Kopie zurück gibt. Sollte auch so gemacht werden, man sollte
-    // nicht die Daten einfach zurückgeben. Deshalb diese Methode
-    // auf die man subscriben kann.
     return this.gamesUpdated.asObservable();
   }
 
