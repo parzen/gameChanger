de75e3b6e0f98a7eacf3c22152bf89bd596d3115
Adrian Tomasek <adrian.tomasek@gmail.com>
2021-09-19 18:01:38 +0200

    fix #4

--- src/app/shared/error-messages/error-messages.ts
+++ src/app/shared/error-messages/error-messages.ts
@@ -1,8 +1,10 @@
 export const errorMessages: { [key: string]: string } = {
   required: 'This field is mandatory.',
-  minPlayersGreaterMaxPlayers: 'Minimal players must be smaller or equal than maximal players.',
-  minPlayTimeGreaterMaxPlayTime: 'Minimal play time must be smaller or equal than maximal play time.',
+  minPlayersGreaterMaxPlayers:
+    'Minimal players must be smaller or equal than maximal players.',
+  minPlayTimeGreaterMaxPlayTime:
+    'Minimal play time must be smaller or equal than maximal play time.',
   minZero: 'The minimal value is 0.',
   minOne: 'The minimal value is 1.',
-  minLength2: 'The minimal length is 2.'
+  minLength2: 'The minimal length is 2.',
 };
