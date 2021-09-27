46d1bf92e06b7e2eb5ad6fe647e25b40dabc4e34
Adrian Tomasek <adrian.tomasek@gmail.com>
2021-09-27 12:10:36 +0200

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+function print_usage() {
+  echo "Usage: $0 [arg]"
+  echo "arg: "
+  echo "  patch :  Increment third digit x.x.2 (default)"
+  echo "  minor :  Increment middle digit x.2.x"
+  echo "  major :  Increment first digit 2.0.0"
+  exit 0
+}
+
+if [ "$1" == "-h" ]
+then
+  print_usage
+fi
+
+update_type="patch"
+if [ "$1" != "" ]
+then
+  update_type=$1
+fi
+
+if [ "${update_type}" != "major" -a "${update_type}" != "minor" -a "${update_type}" != "patch" ]
+then
+  echo "Error: Wrong argument!"
+  print_usage
+fi
+
+npm version ${update_type}
+#git checkout release
+#git merge master
+#git push
+#git checkout master
