c719094852487ebf79fd8df22edf1101fd4d9e18
Adrian <adrian.tomasek@gmail.com>
2021-10-10 10:18:17 +0200

    - update e2e script - remove start_db.sh

--- e2e.sh
+++ e2e.sh
@@ -1,13 +1,52 @@
 #!/bin/bash
 
-cd backend
+function print_usage() {
+  echo "Usage: $0 [arg]"
+  echo "arg: "
+  echo "  local  :  Use local mongo installation"
+  echo "  docker :  Use mongo docker image (default)"
+  exit 0
+}
 
-echo ">>> Running local mongodb"
-./start_db.sh
+if [ "$1" == "-h" ]
+then
+  print_usage
+fi
+
+mongo_installation="docker"
+if [ "$1" != "" ]
+then
+  mongo_installation=$1
+fi
+
+
+echo ">>> Running mongodb server"
+if [ "${mongo_installation}" = "local" ]
+then
+  # With local installation (perhaps need sudo)
+  mongo --version
+  systemctl start mongod
+  systemctl status mongod
+elif [ "${mongo_installation}" = "docker" ]
+then
+  # Create mongo docker
+  #sudo docker run -d --name mongo-on-docker \
+  #  -p 27017:27017 \
+  #  -e MONGO_INITDB_ROOT_USERNAME=mongoadmin \
+  #  -e MONGO_INITDB_ROOT_PASSWORD=secret \
+  #  mongo
+
+  # Start docker
+  sudo docker start mongo-on-docker
+else
+  echo "Error: Wrong argument!"
+  print_usage
+fi
 
 echo ">>> Starting backend server"
-npm run start:testing
-
+cd backend
+npm run start:testing &
 cd ..
+
 echo ">>> Starting frontend server and e2e test"
 npm run e2e:ci
