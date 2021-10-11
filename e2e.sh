09e7d4d420c2f8b41dd595626bf3c93d9b703fb6
Adrian <adrian.tomasek@gmail.com>
2021-10-11 17:55:19 +0200

    update e2e.sh

--- e2e.sh
+++ e2e.sh
@@ -8,29 +8,32 @@ function print_usage() {
   exit 0
 }
 
-if [ "$1" == "-h" ]
-then
+if [ "$1" == "-h" ]; then
   print_usage
 fi
 
 mongo_installation="docker"
-if [ "$1" != "" ]
-then
+if [ "$1" != "" ]; then
   mongo_installation=$1
 fi
 
-
 echo ">>> Running mongodb server"
-if [ "${mongo_installation}" = "local" ]
-then
+if [ "${mongo_installation}" = "local" ]; then
   echo ">>>> Local installation"
   # With local installation (perhaps need sudo)
-  npm install -g mongodb
-  mongod --version
-  systemctl start mongod
-  systemctl status mongod
-elif [ "${mongo_installation}" = "docker" ]
-then
+  cmd="mongod --version"
+  echo $cmd
+  eval $cmd
+  cmd="service mongodb start"
+  echo $cmd
+  eval $cmd
+  cmd="service mongodb status"
+  echo $cmd
+  eval $cmd
+  cmd="mongo --eval 'db.runCommand({ connectionStatus: 1 })'"
+  echo $cmd
+  eval $cmd
+elif [ "${mongo_installation}" = "docker" ]; then
   echo ">>>> Docker installation"
   # Create mongo docker
   #sudo docker run -d --name mongo-on-docker \
@@ -40,7 +43,9 @@ then
   #  mongo
 
   # Start docker
-  sudo docker start mongo-on-docker
+  cmd="sudo docker start mongo-on-docker"
+  echo $cmd
+  eval $cmd
 else
   echo "Error: Wrong argument!"
   print_usage
@@ -48,11 +53,15 @@ fi
 
 echo ">>> Starting backend server"
 cd backend
-npm run start:testing &
+cmd="npm run start:testing &"
+echo $cmd
+eval $cmd
 cd ..
 
 echo ">>> Starting frontend server and e2e test"
-npm run e2e:ci
+cmd="npm run e2e:ci"
+echo $cmd
+eval $cmd
 
 echo ">>> Stopping backend server"
 npm i kill-port
