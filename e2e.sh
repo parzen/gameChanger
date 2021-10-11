18cb7925588afad6f55aacaf80cd2f69c29e701e
Adrian <adrian.tomasek@gmail.com>
2021-10-11 18:55:05 +0200

    - dissable unittest for now - added call_cmd

--- e2e.sh
+++ e2e.sh
@@ -8,6 +8,11 @@ function print_usage() {
   exit 0
 }
 
+function call_cmd() {
+  echo ">>>>> CMD: $1"
+  eval $1
+}
+
 if [ "$1" == "-h" ]; then
   print_usage
 fi
@@ -21,18 +26,11 @@ echo ">>> Running mongodb server"
 if [ "${mongo_installation}" = "local" ]; then
   echo ">>>> Local installation"
   # With local installation (perhaps need sudo)
-  cmd="mongod --version"
-  echo $cmd
-  eval $cmd
-  cmd="service mongodb start"
-  echo $cmd
-  eval $cmd
-  cmd="service mongodb status"
-  echo $cmd
-  eval $cmd
-  cmd="mongo --eval 'db.runCommand({ connectionStatus: 1 })'"
-  echo $cmd
-  eval $cmd
+  call_cmd "mongod --version"
+  call_cmd "mongo --eval 'db.createUser({ user: \"mongoadmin\", pwd: \"secret\" ]})'"
+  call_cmd "service mongod start"
+  call_cmd "service mongod status"
+  call_cmd "mongo --eval 'db.runCommand({ connectionStatus: 1 })'"
 elif [ "${mongo_installation}" = "docker" ]; then
   echo ">>>> Docker installation"
   # Create mongo docker
@@ -43,9 +41,7 @@ elif [ "${mongo_installation}" = "docker" ]; then
   #  mongo
 
   # Start docker
-  cmd="sudo docker start mongo-on-docker"
-  echo $cmd
-  eval $cmd
+  call_cmd "sudo docker start mongo-on-docker"
 else
   echo "Error: Wrong argument!"
   print_usage
@@ -53,15 +49,11 @@ fi
 
 echo ">>> Starting backend server"
 cd backend
-cmd="npm run start:testing &"
-echo $cmd
-eval $cmd
+call_cmd "npm run start:testing &"
 cd ..
 
 echo ">>> Starting frontend server and e2e test"
-cmd="npm run e2e:ci"
-echo $cmd
-eval $cmd
+call_cmd "npm run e2e:ci"
 
 echo ">>> Stopping backend server"
 npm i kill-port
